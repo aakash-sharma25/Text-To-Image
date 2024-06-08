b93918da69ce6e0a392f4e33e43ed942235deb70
aakash-sharma25 <as024harma@gmail.com>
2024-06-08 20:42:37 +0530

    Added the component in App.js file

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ImageGenerationForm from "./components/ImageGen";
+import "./index.css";
+function App() {
+  return (
+    <div className="main-div">
+      <ImageGenerationForm />
+    </div>
+  );
+}
+
+export default App;
