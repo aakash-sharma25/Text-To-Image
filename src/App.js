c19aeca16aceda1687636add3f890e194afd79e1
aakash-sharma25 <as024harma@gmail.com>
2024-06-08 20:44:48 +0530

    change the css file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ImageGenerationForm from "./components/ImageGen";
 import "./index.css";
 function App() {
   return (
-    <div className="main-div">
+    <div className=" bg-[#3D52A0] h-[100vh]">
       <ImageGenerationForm />
     </div>
   );
